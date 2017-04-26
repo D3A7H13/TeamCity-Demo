a62636a51794283df6abfe90147c3714cd0d3c7b
Simeon Sheytanov <s_sheitanov@abv.bg>
2017-04-26 18:11:38 +0300

    Initial project

---
+++ src/main/java/softuni/controllers/MathController.java
@@ -0,0 +1,17 @@
+package softuni.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{firstOperand}/{secondOperand}")
+    public String add(@PathVariable Long firstOperand, @PathVariable Long secondOperand, Model model) {
+        model.addAttribute("result", firstOperand + secondOperand);
+
+        return "layout";
+    }
+}
