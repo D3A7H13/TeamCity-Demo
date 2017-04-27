3e1596e24826ac3799692970674f35e8e7eafb94
Simeon Sheytanov <s_sheitanov@abv.bg>
2017-04-27 13:50:59 +0300

    normal spring app

--- src/main/java/softuni/TeamcityApplication.java
+++ src/main/java/softuni/TeamcityApplication.java
@@ -5,23 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-//@SpringBootApplication
-//public class TeamcityApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(TeamcityApplication.class, args);
-//	}
-//}
-
 @SpringBootApplication
-public class TeamcityApplication extends SpringBootServletInitializer{
+public class TeamcityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TeamcityApplication.class, args);
 	}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(TeamcityApplication.class);
-    }
 }
+
+//@SpringBootApplication
+//public class TeamcityApplication extends SpringBootServletInitializer{
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(TeamcityApplication.class, args);
+//	}
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(TeamcityApplication.class);
+//    }
+//}
